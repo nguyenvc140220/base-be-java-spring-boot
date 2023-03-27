3dd98cf54f863dea283cf33f21e089e8ea3541e9
Doan Tien <tiendc@metechvn.com>
2023-03-27 09:24:19 +0700

    add api to create entity

--- src/main/java/com/metechvn/dynamic/repositories/DynamicEntityRepository.java
+++ src/main/java/com/metechvn/dynamic/repositories/DynamicEntityRepository.java
@@ -1,9 +1,18 @@
 package com.metechvn.dynamic.repositories;
 
 import com.metechvn.dynamic.entities.DynamicEntity;
+import com.metechvn.dynamic.entities.DynamicEntityType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.UUID;
 
 public interface DynamicEntityRepository extends JpaRepository<DynamicEntity, UUID> {
+    @Query("select det from DynamicEntity det " +
+            " left join fetch det.entityType et " +
+            " left join fetch det.properties dp " +
+            " left join fetch dp.entityPropertyValue dpv " +
+            " where det.id = ?1")
+    DynamicEntity findIncludeRelationsById(UUID id);
+
 }
