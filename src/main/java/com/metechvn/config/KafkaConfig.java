3530757e9bd48d1e0566a0dc36a7c695b32feebd
__Luong <dot@luongmb.local>
2023-03-30 13:38:12 +0700

    json hybrid deserializer

--- src/main/java/com/metechvn/config/KafkaConfig.java
+++ src/main/java/com/metechvn/config/KafkaConfig.java
@@ -1,16 +1,15 @@
 package com.metechvn.config;
 
+import com.metechvn.common.kafka.JsonHybridDeserializer;
 import io.confluent.kafka.serializers.json.KafkaJsonSchemaDeserializer;
 import io.confluent.kafka.serializers.json.KafkaJsonSchemaSerializer;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.*;
-import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.Map;
@@ -72,8 +71,8 @@ public class KafkaConfig {
     @Bean(name = "objConsumerFactory")
     public ConsumerFactory<Object, Object> objConsumerFactory() {
         final var properties = this.consumerProperties;
-        properties.put(KEY_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class.getName());
-        properties.put(VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class.getName());
+        properties.put(KEY_DESERIALIZER_CLASS_CONFIG, JsonHybridDeserializer.class.getName());
+        properties.put(VALUE_DESERIALIZER_CLASS_CONFIG, JsonHybridDeserializer.class.getName());
 
         if (StringUtils.isNotEmpty(schemaRegistryUrl)) {
             properties.put(KEY_SERIALIZER_CLASS_CONFIG, KafkaJsonSchemaDeserializer.class.getName());
@@ -88,29 +87,6 @@ public class KafkaConfig {
             ConsumerFactory<Object, Object> objConsumerFactory) {
         var factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(objConsumerFactory);
-
-        return factory;
-    }
-
-    @Bean(name = "strKeyObjConsumerFactory")
-    public ConsumerFactory<String, Object> strKeyObjConsumerFactory() {
-        final var properties = this.consumerProperties;
-        properties.put(KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.put(VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class.getName());
-
-        if (StringUtils.isNotEmpty(schemaRegistryUrl)) {
-            properties.put(KEY_SERIALIZER_CLASS_CONFIG, KafkaJsonSchemaDeserializer.class.getName());
-            properties.put(VALUE_SERIALIZER_CLASS_CONFIG, KafkaJsonSchemaDeserializer.class.getName());
-        }
-
-        return new DefaultKafkaConsumerFactory<>(properties);
-    }
-
-    @Bean(name = "objListenerContainerFactory")
-    public ConcurrentKafkaListenerContainerFactory<String, Object> strKeyObjContainerFactory(
-            ConsumerFactory<String, Object> strKeyObjConsumerFactory) {
-        var factory = new ConcurrentKafkaListenerContainerFactory<String, Object>();
-        factory.setConsumerFactory(strKeyObjConsumerFactory);
 
         return factory;
     }
