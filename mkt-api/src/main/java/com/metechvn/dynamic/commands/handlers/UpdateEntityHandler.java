5aa5eb8d85aef0792b8878367e18278beea396f6
Vũ Cao Nguyên <nguyenvc@metechvn.com>
2023-04-05 11:39:35 +0700

    fixbug/MKT-63-fix-sync-data

--- mkt-api/src/main/java/com/metechvn/dynamic/commands/handlers/UpdateEntityHandler.java
+++ mkt-api/src/main/java/com/metechvn/dynamic/commands/handlers/UpdateEntityHandler.java
@@ -7,13 +7,21 @@ import com.metechvn.dynamic.entities.DynamicEntity;
 import com.metechvn.dynamic.repositories.DynamicEntityRepository;
 import com.metechvn.dynamic.repositories.DynamicEntityTypeRepository;
 import com.metechvn.exception.BusinessException;
+import com.metechvn.validators.IDynamicTypeValidator;
+import com.metechvn.validators.dtos.DynamicTypeValidator;
+import com.metechvn.validators.dtos.DynamicTypeValidatorDto;
 import lombok.RequiredArgsConstructor;
 import luongdev.cqrs.RequestHandler;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Component
 @RequiredArgsConstructor
 public class UpdateEntityHandler implements RequestHandler<DynamicEntity, UpdateEntityCommand> {
+    private final IDynamicTypeValidator validator;
     private final CurrentTenantProvider currentTenantProvider;
     private final DynamicEntityRepository dynamicEntityRepository;
     private final DynamicEntityTypeRepository dynamicEntityTypeRepository;
@@ -21,20 +29,33 @@ public class UpdateEntityHandler implements RequestHandler<DynamicEntity, Update
     public DynamicEntity handle(UpdateEntityCommand cmd) {
         var typeIncludeProps = dynamicEntityTypeRepository.findIncludeRelationsByCode(cmd.getCode());
         var entity = dynamicEntityRepository.findById(cmd.getIdEntity());
-        if (typeIncludeProps == null && entity == null)
+        if (typeIncludeProps == null || entity == null)
             throw new BusinessException(String.format("Cannot find object named '%s' or entityID '$s'", cmd.getCode(), cmd.getIdEntity()));
 
-        var entityGet= entity.get();
+        var entityGet = new DynamicEntity();
         entityGet.setEntityType(typeIncludeProps);
+        entityGet.setId(cmd.getIdEntity());
         entityGet.setTenant(currentTenantProvider.getTenant());
+
+        var validators = typeIncludeProps.getProperties().entrySet()
+                .stream()
+                .filter(e -> StringUtils.hasText(e.getValue().getProperty().getValidators()))
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        e -> DynamicTypeValidator.fromJson(e.getValue().getProperty().getValidators())
+                ));
 
         for (var entry : cmd.getProperties().entrySet()) {
             if (!typeIncludeProps.exists(entry.getKey())) continue;
 
-            // validate value
-            //...
+            var propValidators = validators.get(entry.getKey());
+            if (propValidators != null && !propValidators.isEmpty()) {
+                var validator = new DynamicTypeValidatorDto(entry.getKey(), entry.getValue(), propValidators);
+                validator.setValidators(propValidators);
 
-            // set value
+                this.validator.test(validator);
+            }
+
             entityGet.set(typeIncludeProps.getProperty(entry.getKey()), entry.getValue());
         }
 
