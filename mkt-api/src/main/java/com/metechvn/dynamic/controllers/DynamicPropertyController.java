f201b2e2affe30c5456043986742858ef4ef2f2d
longhm <longhm@LongHM>
2023-04-19 16:04:53 +0700

    update request body to param

--- mkt-api/src/main/java/com/metechvn/dynamic/controllers/DynamicPropertyController.java
+++ mkt-api/src/main/java/com/metechvn/dynamic/controllers/DynamicPropertyController.java
@@ -33,8 +33,8 @@ public class DynamicPropertyController {
     }
 
     @DeleteMapping({""})
-    public BaseResponse<Boolean> deleteProperty(@Valid @RequestBody DeletePropertyCommand cmd) {
-        System.out.println(bus.execute(cmd));
+    public BaseResponse<Boolean> deleteProperty(@RequestParam("code") String code) {
+        System.out.println(bus.execute(new DeletePropertyCommand(code)));
         return BaseResponse.onOk(true);
     }
 
