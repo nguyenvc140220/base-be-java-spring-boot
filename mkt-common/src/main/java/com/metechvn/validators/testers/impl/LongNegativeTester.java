40c25d8a5cffd01684159a929e04aae8b65abf55
__Luong <dot@luongmb.local>
2023-04-28 09:57:22 +0700

    not required check range, size, ... when null

--- mkt-common/src/main/java/com/metechvn/validators/testers/impl/LongNegativeTester.java
+++ mkt-common/src/main/java/com/metechvn/validators/testers/impl/LongNegativeTester.java
@@ -3,6 +3,7 @@ package com.metechvn.validators.testers.impl;
 
 import com.metechvn.validators.exceptions.DynamicTypeValidatorException;
 import com.metechvn.validators.testers.ITester;
+import org.springframework.util.StringUtils;
 
 public class LongNegativeTester implements ITester {
     /**
@@ -14,6 +15,8 @@ public class LongNegativeTester implements ITester {
      */
     @Override
     public boolean test(String validatorValue, String testValue) throws DynamicTypeValidatorException {
+        if (!StringUtils.hasText(testValue)) return true;
+
         if ("1".equals(validatorValue)) {
             try {
                 long testValueLong = Long.parseLong(testValue);
